07c6c049fb570541d98d586ec0e5e55d2bb8ef00
Ravi Shankar <ravishankar@Ravis-Personal-Macbook-pro.local>
2024-10-08 00:32:21 +0530

    added code for chapter 1.1, to create vulkan instance

---
+++ src/tutorials/instance/01_1_vulkan_instance.rs
@@ -0,0 +1,41 @@
+use std::ffi::CString;
+use ash::{vk, Entry, Instance};
+
+fn main() {
+    let entry = unsafe { Entry::load().expect("Failed to create entry.") };
+    let instance = create_instance(&entry);
+
+    println!("vulkan instance created");
+}
+
+fn create_instance(entry: &Entry) -> Instance {
+    let app_name = CString::new("Vulkan Application").unwrap();
+    let engine_name = CString::new("No Engine").unwrap();
+    let app_info = vk::ApplicationInfo::default()
+        .application_name(app_name.as_c_str())
+        .application_version(vk::make_api_version(0, 0, 1, 0))
+        .engine_name(engine_name.as_c_str())
+        .engine_version(vk::make_api_version(0, 0, 1, 0))
+        .api_version(vk::make_api_version(0, 1, 0, 0));
+
+    let mut extension_names: Vec<*const i8> = Vec::new();
+
+    #[cfg(any(target_os = "macos", target_os = "ios"))]
+    {
+        extension_names.push(ash::khr::portability_enumeration::NAME.as_ptr());
+        // Enabling this extension is a requirement when using `VK_KHR_portability_subset`
+        extension_names.push(ash::khr::get_physical_device_properties2::NAME.as_ptr());
+    }
+
+    let create_flags = if cfg!(any(target_os = "macos", target_os = "ios")) {
+        vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+    } else {
+        vk::InstanceCreateFlags::default()
+    };
+    let mut instance_create_info = vk::InstanceCreateInfo::default()
+        .application_info(&app_info)
+        .enabled_extension_names(&extension_names)
+        .flags(create_flags);
+
+    unsafe { entry.create_instance(&instance_create_info, None).unwrap() }
+}
